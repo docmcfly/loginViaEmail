beaf5e711ce0459858210c5e0896a56ab68b5b11
clemens <clemens@miffy-lx-II>
2020-04-25 18:42:01 +0200

    Corrects the description and meta data

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'LoginViaEMail',
     'description' => 'EN:
-The \"Frontend user login\" extension in the core can only be logged in with the user name.  The e-mail address that the user has entered in the personal data cannot be used. 
+The "Frontend user login" extension in the core can only be logged in with the user name.  The e-mail address that the user has entered in the personal data cannot be used. 
 
 This extension extends the authorization mechanism: 
 
@@ -22,7 +22,7 @@ If an email address is entered as the username, the corresponding user is identi
 Note: This extension also affects the login via URL. In other words, you can also use an e-mail address for login in the URL. 
 
 DE: 
-Das im Core befindlichen \"Frontend user login\"-Erweiterung kann man sich nur mit dem Nutzernamen einloggen.  Die E-Mail-Adresse, die der Nutzer in den persönlichen Daten hinterlegt hat, kann man nicht verwenden. 
+Das im Core befindlichen "Frontend user login"-Erweiterung kann man sich nur mit dem Nutzernamen einloggen.  Die E-Mail-Adresse, die der Nutzer in den persönlichen Daten hinterlegt hat, kann man nicht verwenden. 
 
 Diese Erweiterung erweitert den Authorisationsmechanismus auf: 
 
@@ -31,13 +31,13 @@ Wenn eine E-Mail-Adresse als Nutzername angeben wurde, wird der dazugehörige Nu
 Hinweis: Diese Erweiterung wirkt sich auch auf den Login via URL aus. Sprich: man kann dann auch eine E-Mail-Adresse fürs Login in der URL verwenden. 
 ',
     'category' => 'misc',
-    'author' => 'Clemens Gogolin, Clemens Gogolin',
-    'author_email' => 'service@cylancer.net, service@cylancer.net',
+    'author' => 'Clemens Gogolin',
+    'author_email' => 'service@cylancer.net',
     'state' => 'beta',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '0.9.9',
+    'version' => '0.9.10',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-9.5.99',
