29c998bc37659dc1b64046586db4c1d5fe41b6c4
Clemens Gogolin <service@cylancer.net>
2020-04-30 17:38:34 +0200

    - Version 8.7 allowed, thanks frikas - spelling error fixed

--- loginViaEmail/ext_tables.php
+++ loginViaEmail/ext_tables.php
@@ -4,8 +4,7 @@ defined('TYPO3_MODE') || die('Access denied.');
 call_user_func(
     function()
     {
-
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('loginviaemail', 'Configuration/TypoScript', 'LoginViaEMail');
-
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+                'loginviaemail', 'Configuration/TypoScript', 'LoginViaEMail');
     }
 );
