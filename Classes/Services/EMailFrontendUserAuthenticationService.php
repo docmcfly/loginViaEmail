e0ed2e45a7b38f8ad7a0643c0d698ca76ba20fb0
Clemens <git@cylancer.net>
2023-02-19 12:06:06 +0100

    Update the extension icon (#12)
    Co-authored-by: Clemens Gogoljn <service@cylancer.net>

--- Classes/Services/EMailFrontendUserAuthenticationService.php
+++ Classes/Services/EMailFrontendUserAuthenticationService.php
@@ -5,22 +5,6 @@ use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
 use TYPO3\CMS\Core\Crypto\PasswordHashing\PasswordHashFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-/*
- * EMailFrontendUserAuthenticationService.php
- *
- * Copyright 2022 Clemens Gogolin <service@cylancer.net>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- */
 class EMailFrontendUserAuthenticationService extends FrontendUserAuthentication
 {
 
@@ -117,7 +101,8 @@ class EMailFrontendUserAuthenticationService extends FrontendUserAuthentication
      */
     private function checkPassword($password, $passwordHash)
     {
-        return GeneralUtility::makeInstance(PasswordHashFactory::class)->get($passwordHash, $this->loginType)->checkPassword($password, $passwordHash);
+        return GeneralUtility::makeInstance(PasswordHashFactory::class)->get($passwordHash, $this->loginType)
+            ->checkPassword($password, $passwordHash);
     }
 }
 
