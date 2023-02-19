549418b2101012facce755dc47ca841fa96d9ca0
Clemens Gogoljn <service@cylancer.net>
2023-02-19 12:02:21 +0100

    Update the extension icon

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,18 +1,18 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "loginviaemail".
+/**
+ * This file is part of the "login via email" Extension for TYPO3 CMS.
  *
- * Auto generated 22-06-2020 17:50
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
+ * (c) 2023 Clemens Gogolin <service@cylancer.net>
+ * 
+ */
 
-$EM_CONF[$_EXTKEY] = array (
-  'title' => 'LoginViaEMail',
-  'description' => 'EN:
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'LoginViaEMail',
+    'description' => 'EN:
 With the "Frontend user login" extension in the core, you can only log in with the user name. The e-mail address that the user has entered in the 
 personal data cannot be used.
     
@@ -29,27 +29,28 @@ Diese Erweiterung erweitert den Authorisationsmechanismus:
     
 Wenn eine E-Mail-Adresse als Nutzername angeben wurde, wird der dazugehörige Nutzer identifiziert. Nur wenn der Nutzername 
 eindeutig zur E-Mail-Adresse zugeordnet werden kann, wird dem Authorisationsmechanismus der Nutzername übergeben und mit diesem das Login durchgeführt.',
-  'category' => 'misc',
-  'author' => 'Clemens Gogolin',
-  'author_email' => 'service@cylancer.net',
-  'state' => 'stable',
-  'uploadfolder' => false,
-  'clearCacheOnLoad' => 0,
-  'version' => '3.0.0',
-  'constraints' => 
-  array (
-    'depends' => 
-    array (
-      'typo3' => '11.5.12-11.5.99',
-    ),
-    'conflicts' => 
-    array (
-    ),
-    'suggests' => 
-    array (
-    ),
-  ),
-  'clearcacheonload' => false,
-  'author_company' => NULL,
+    'category' => 'misc',
+    'version' => '3.0.1',
+    'state' => 'stable',
+    'uploadfolder' => false,
+    'clearcacheonload' => false,
+    'author' => 'Clemens Gogolin',
+    'author_email' => 'service@cylancer.net',
+    'author_company' => NULL,
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '11.5.0-11.5.99'
+        ),
+        'conflicts' => array(),
+        'suggests' => array()
+    )
 );
+
+
+/* ---- CHANGELOG ---------- 
+ 
+ 3.0.0 :: Initial
+ 3.0.1 :: Update the extension icon
+ 
+   ---- CHANGELOG ---------- */
 
