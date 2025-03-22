858284ab950c66a0c45d1676777771889b126fc8
Clemens <git@cylancer.net>
2025-03-22 16:47:18 +0100

    Develop (#23)
    * Update the extension icon
    
    * Fix the composer
    
    * Fix #2
    
    * Fix #3
    
    * Small database query optimization
    
    * TYPO3 12.4 compatibility
    
    * TYPO3 12.4 compatibility
    
    * TYPO3 13.4 compatibility
    
    ---------
    
    Co-authored-by: Clemens Gogoljn <service@cylancer.net>

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,24 +13,27 @@
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'LoginViaEMail',
     'description' => 'EN:
-With the "Frontend user login" extension in the core, you can only log in with the user name. The e-mail address that the user has entered in the 
-personal data cannot be used.
+With the "Frontend user login" extension in the core, you can only log in with the user name. The e-mail 
+address that the user has entered in the  personal data cannot be used.
     
 This extension extends the authorization mechanism:
     
-If an email address is entered as the username, the corresponding user is identified. Only if the user name can be uniquely assigned to the e-mail 
-address will the user name be passed to the authorization mechanism and the login be carried out with it.
+If an email address is entered as the username, the corresponding user is identified. Only if the user 
+name can be uniquely assigned to the e-mail address will the user name be passed to the authorization 
+mechanism and the login be carried out with it.
     
 DE:
-Mit der im Core befindlichen "Frontend user login"-Erweiterung kann man sich nur mit dem Nutzernamen einloggen. 
-Die E-Mail-Adresse, die der Nutzer in den persönlichen Daten hinterlegt hat, kann man nicht verwenden.
+Mit der im Core befindlichen "Frontend user login"-Erweiterung kann man sich nur mit dem 
+Nutzernamen einloggen. Die E-Mail-Adresse, die der Nutzer in den persönlichen Daten hinterlegt hat, 
+kann man nicht verwenden.
     
 Diese Erweiterung erweitert den Authorisationsmechanismus:
     
-Wenn eine E-Mail-Adresse als Nutzername angeben wurde, wird der dazugehörige Nutzer identifiziert. Nur wenn der Nutzername 
-eindeutig zur E-Mail-Adresse zugeordnet werden kann, wird dem Authorisationsmechanismus der Nutzername übergeben und mit diesem das Login durchgeführt.',
+Wenn eine E-Mail-Adresse als Nutzername angeben wurde, wird der dazugehörige Nutzer identifiziert. 
+Nur wenn der Nutzername eindeutig zur E-Mail-Adresse zugeordnet werden kann, wird dem 
+Authorisationsmechanismus der Nutzername übergeben und mit diesem das Login durchgeführt.',
     'category' => 'misc',
-    'version' => '4.0.0',
+    'version' => '5.0.0',
     'state' => 'stable',
     'uploadfolder' => false,
     'clearcacheonload' => false,
@@ -39,7 +42,7 @@ eindeutig zur E-Mail-Adresse zugeordnet werden kann, wird dem Authorisationsmech
     'author_company' => NULL,
     'constraints' => array(
         'depends' => array(
-            'typo3' => '12.4.11-12.4.99'
+            'typo3' => '13.4.4-13.4.99'
         ),
         'conflicts' => array(),
         'suggests' => array()
@@ -49,6 +52,7 @@ eindeutig zur E-Mail-Adresse zugeordnet werden kann, wird dem Authorisationsmech
 
 /* ---- CHANGELOG ---------- 
 
+ 5.0.0 :: TYPO3 13.4 compatibility   
  4.0.0 :: TYPO3 12.4 compatibility   
  3.0.2 :: Small optimation
  3.0.1 :: Update the extension icon
