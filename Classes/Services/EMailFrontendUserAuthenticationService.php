e643bd62d7716d286ee478cbf51c9c2bca10e515
Clemens Gogoljn <service@cylancer.net>
2026-01-15 08:33:50 +0100

    Update year in the class comment

--- Classes/Services/EMailFrontendUserAuthenticationService.php
+++ Classes/Services/EMailFrontendUserAuthenticationService.php
@@ -5,7 +5,7 @@
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
  *
- * (c) 2025 C. Gogolin <service@cylancer.net>
+ * (c) 2026 C. Gogolin <service@cylancer.net>
  *
  */
 
